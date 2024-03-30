54b21de5968087529bfbdf720f98cf260c6e9dde
effilin <effienummer@gmail.com>
2024-03-30 15:00:43 -0400

    js functions complete

--- lab1.2.js
+++ lab1.2.js
@@ -4,3 +4,38 @@ function addIt(a, b) {
 }
 let addition = addIt(2, 4) ;
 document.getElementById("Math").innerHTML= addition;
+
+// which would work better?
+
+function subIt(a, b) {
+    let result = a - b ;
+    document.getElementById("Math").innerHTML = result ;
+}
+
+function isItDivisible(a, b) {
+    if (b == 0) {
+    window.alert("Not Divisible")
+    }
+    else { 
+        let result = a / b ;
+        document.getElementById("Math").innerHTML= result ;
+    }
+}
+
+function multiplier(a) {
+    for(let i = 0; i < 5; i++) {
+        let results = a * i ;
+        document.getElementById("Math").innerHTML= results + "<br>" ;
+    }
+
+}
+
+function Hello() {
+    let text ;
+    let person = window.prompt("What's Your Name?", "T-Rex Ron")
+    if(person == ""){
+        text = "Canceled"
+    } else {
+        text = "Hello" + person + "!"
+    }
+}git
